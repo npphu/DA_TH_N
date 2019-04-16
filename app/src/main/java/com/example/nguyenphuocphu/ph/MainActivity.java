c4cd5af35bf137e3ba87ed7b470f101705fb358c
Nguyen Phu <nguyenphuocphu@outlook.com>
2019-04-16 23:16:04 +0700

    test push

--- app/src/main/java/com/example/nguyenphuocphu/ph/MainActivity.java
+++ app/src/main/java/com/example/nguyenphuocphu/ph/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test push 
-
 
     }
 }
